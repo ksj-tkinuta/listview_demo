c190db57290ba1c352d1042bee9ca893a8e86f4e
t.kinuta <takashi.kinuta@bluetec.co.jp>
2024-01-25 11:54:01 +0900

    add ListView.builder

--- lib/my_list_page.dart
+++ lib/my_list_page.dart
@@ -1,40 +1,60 @@
 import 'package:flutter/material.dart';
 
 class MyListPage extends StatelessWidget {
-  const MyListPage({super.key});
+  MyListPage({super.key});
+
+  final List<String> entries = <String>[
+    'A',
+    'B',
+    'C',
+    'A',
+    'B',
+    'C',
+    'A',
+    'B',
+    'C',
+    'A',
+    'B',
+    'C'
+  ];
+  final List<int> colorCodes = <int>[
+    600,
+    500,
+    100,
+    600,
+    500,
+    100,
+    600,
+    500,
+    100,
+    600,
+    500,
+    100
+  ];
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
         title: const Text('ListView'),
       ),
       body: Container(
-          child: ListView(
-            padding: const EdgeInsets.all(8),
-            children: <Widget>[
-              Container(
-                height: 50,
-                color: Colors.amber[600],
-                child: const Center(child: Text('Entry A')),
-              ),
-              Container(
-                height: 50,
-                color: Colors.amber[500],
-                child: const Center(child: Text('Entry B')),
-              ),
-              Container(
-                height: 50,
-                color: Colors.amber[100],
-                child: const Center(child: Text('Entry C')),
-              ),
-            ],
-          )
-      ),
+          child: ListView.builder(
+              padding: const EdgeInsets.all(8),
+              itemCount: entries.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 50,
+                    color: Colors.amber[colorCodes[index]],
+                    child: Center(child: Text('Entry ${entries[index]}',style: TextStyle(
+                      fontSize: 25
+                    ),)),
+                  ),
+                );
+              })),
       // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
